cdb4efff6811d010d6891038dc6b3540db901831
Gabriel Falcao <gabriel@nacaolivre.org>
2011-11-21 04:47:48 -0200

    playing with socket.io

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,19 @@
 $(function(){
+    var socket = io.connect();
+
     $("div.build.warning.block-message pre").effect("pulsate", {times: 50}, 1000);
+    socket.on('User deleted', function(data){
+        $(".clay.User.delete[rel=" + data.id + "]").parent().remove();
+    });
+
+    socket.on('connected', function (data) {
+        $(".clay.User.delete").live("click", function() {
+            var id = $(this).attr("rel");
+            socket.emit('delete User', {id: id});
+            $(this).parent().animate({
+                'opacity': 0.2,
+            })
+            return false;
+        });
+    });
 });
