a016ef29077a7020de103d8de1f2684173aec14c
Gabriel Falcao <gabriel@nacaolivre.org>
2011-11-21 04:47:38 -0200

    cleanup in models

--- models.js
+++ models.js
@@ -1,19 +1,11 @@
 var models = require('clay');
-
-var User = models.declare("User", function(it, kind){
-    it.has.field("name", kind.string);
-    it.has.field("email", kind.email);
-    it.has.field("password", kind.hashOf(["name", "email"]));
-    it.has.field("oauth_token", kind.string);
-
-    it.validates.uniquenessOf("name");
-    it.validates.uniquenessOf("email");
-});
+var crypto = require('crypto');
 
 var Build = models.declare("Build", function(it, kind){
     it.has.field("status", kind.numeric);
     it.has.field("error", kind.string);
     it.has.field("output", kind.string);
+    it.has.field("pid", kind.number);
     it.has.field("commit", kind.string);
     it.has.field("author_name", kind.string);
     it.has.field("author_email", kind.string);
@@ -29,12 +21,14 @@ var BuildInstruction = models.declare("BuildInstruction", function(it, kind){
 
     it.has.index("repository_address");
     it.has.many("builds", Build, "instruction");
-    it.has.one("owner", User, "build_instructions");
+
+    it.has.getter('permalink', function(){
+        return '/instruction/' + this.__id__;
+    });
 });
 
 module.exports.prepare = function(io, app){
     return {
-        User: User,
         BuildInstruction: BuildInstruction,
         Build: Build
     }
