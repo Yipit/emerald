1bb79beaaf02d7d8b22d385c6dc6d2f7ba005e55
Gabriel Falcao <gabriel@nacaolivre.org>
2011-12-11 02:41:50 -0200

    code cleanup in websockets.js

--- websockets.js
+++ websockets.js
@@ -1,7 +1,7 @@
 var entity = require("./models");
 var settings = require('./settings');
 
-var logger = new (require('./logger').Logger)("[websockets]".yellow);
+var logger = new (require('./logger').Logger)("[WEBSOCKET]".blue.bold);
 
 exports.work_on = function(redis, io) {
     io.sockets.on('connection', function (socket) {
@@ -9,22 +9,19 @@ exports.work_on = function(redis, io) {
 
         require('./pubsub').use(redis, socket);
 
-        socket.on('delete BuildInstruction', function (data) {
-            entity.BuildInstruction.find_by_id(parseInt(data.id), function(err, instruction){
-                logger.handleException("entity.BuildInstruction.find_by_id", err);
-                instruction.delete(function(){
-                    socket.emit('BuildInstruction deleted', {id: instruction.__id__});
+        ['BuildInstruction', 'User'].forEach(function(ModelName){
+            socket.on('delete ' + ModelName, function (data) {
+                entity[ModelName].find_by_id(parseInt(data.id), function(err, instance){
+                    logger.handleException("entity." + ModelName + ".find_by_id", err);
+                    instance.delete(function(err){
+                        logger.handleException("(new " + ModelName + ")({__id__:" + data.id + "}).delete():", err);
+                        logger.info([ModelName, "#" + data.id, "deleted"]);
+                        socket.emit(ModelName + ' deleted', {id: data.id});
+                    });
                 });
             });
         });
-        socket.on('delete User', function (data) {
-            entity.User.find_by_id(parseInt(data.id), function(err, instruction){
-                logger.handleException("entity.User.find_by_id", err);
-                instruction.delete(function(){
-                    socket.emit('User deleted', {id: instruction.__id__});
-                });
-            });
-        });
+
         socket.on('run BuildInstruction', function (data) {
             logger.info("enqueuing instruction #" + data.id);
             redis.zincrby(settings.REDIS_KEYS.build_queue, 1, data.id, function(err){
