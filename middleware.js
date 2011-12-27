4213fc5c3d51beffff9907f4c566ec7e710490a9
Gabriel Falcao <gabriel@nacaolivre.org>
2011-12-27 14:20:33 -0500

    merging objects properly in `response.show`

--- middleware.js
+++ middleware.js
@@ -5,25 +5,22 @@ var entity = require('./models');
 var settings = require('./settings');
 
 exports.authentication = function(request, response, next) {
-    fs.readFile(settings.LOCAL_FILE('.terminal_example.txt'), function(err, raw_terminal_example) {
-        entity.User.find_by_id(request.session.user_id, function(err, user) {
-            response.show = function (name, context) {
-                var emerald_meta = {
-                    domain: settings.EMERALD_DOMAIN,
-                    hostname: settings.EMERALD_HOSTNAME,
-                    port: settings.EMERALD_PORT
-                };
-                var c = _.extend(context || {}, {
-                    title: 'Emerald - Continuous Integration',
-                    request: request,
-                    user: user,
-                    emerald: emerald_meta,
-                    emerald_json: JSON.stringify(emerald_meta),
-                    terminal_example: ansispan(raw_terminal_example.toString())
-                });
-                response.render(name, c);
-            }
-            return next();
-        });
+    entity.User.find_by_id(request.session.user_id, function(err, user) {
+        response.show = function (name, context) {
+            var emerald_meta = {
+                domain: settings.EMERALD_DOMAIN,
+                hostname: settings.EMERALD_HOSTNAME,
+                port: settings.EMERALD_PORT
+            };
+            var c = _.extend({
+                title: 'Emerald - Continuous Integration',
+                request: request,
+                user: user,
+                emerald: emerald_meta,
+                emerald_json: JSON.stringify(emerald_meta),
+            }, context || {});
+            response.render(name, c);
+        }
+        return next();
     });
 }
