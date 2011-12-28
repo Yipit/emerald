979a91495f65f562928580fc8f25992cc65ec78e
Gabriel Falcao <gabriel@nacaolivre.org>
2011-12-28 00:05:28 -0500

    running the first full-length build

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 var models = require('clay');
 var crypto = require('crypto');
+var mkdirp = require('mkdirp');
 var async = require('async');
 var path = require('path');
 var fs = require('fs');
@@ -93,52 +94,50 @@ var BuildInstruction = models.declare("BuildInstruction", function(it, kind) {
     it.has.method('run', function(current_build, lock) {
         var self = this;
 
+        var hash = crypto.createHash('md5');
+        hash.update((new Date()).toString() + self.name);
+        function filter_output (text) {
+            return text.replace(/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]/g, '');
+        }
         var start_time = new Date();
         var redis = self._meta.storage.connection;
         /* TODO: extract the repo name and check if already exists e*/
         var repository_folder_name = (self.repository_address + self.name).replace(/\W+/g, '');
         var repository_full_path = path.join(settings.SANDBOX_PATH, repository_folder_name);
+        var repository_bare_path = path.join(repository_full_path, '.git');
+
+        var script_name = hash.digest('hex') + '.sh';
+        var script_path = path.join(repository_full_path, script_name);
+
         var branch_to_build = self.branch || "master";
         redis.publish("Build started", JSON.stringify({
             build: current_build.__data__,
             instruction: self.__data__
         }));
         async.waterfall([
-            function(callback){
+            function decide_whether_pull_or_clone (callback){
                 logger.info('preparing to fetch data from "'+self.name+'" through "'+self.repository_address+'@'+branch_to_build+'" at ' + repository_full_path);
-                require('path').exists(repository_full_path, function(exists){callback(null, exists)})
+                path.exists(repository_bare_path, function(exists){callback(null, exists)})
             },
-            function(exists, callback) {
+            function assemble_the_command_line (exists, callback) {
                 var args, options = {};
                 if (exists) {
                     args = ["pull", "origin", branch_to_build || "master"];
                     options.cwd = repository_full_path;
-                    logger.info('found an existing folder at "'+repository_full_path+'", gonna use git-pull');
+                    logger.info('found an existing git repo at "'+repository_bare_path+'", gonna use git-pull');
                 } else {
-                    args = ["clone", "--progress", self.repository_address, repository_folder_name];
+                    args = ["clone", self.repository_address, repository_folder_name];
                     logger.info('local copy does not exist, will clone at "'+repository_full_path+'"');
                     options.cwd = settings.SANDBOX_PATH;
                 }
                 callback(null, args, options);
             },
-            function spawn_the_command(command_args, command_options, callback){
+            function spawn_git(command_args, command_options, callback){
                 logger.info('spawning "git '+command_args.join(' ')+'"');
                 var command = child_process.spawn("git", command_args, command_options);
-                callback(null, command);
+                callback(null, command, command_args);
             },
-            function listen_to_stdout(command, callback){
-                command.stdout.on('data', function (data) {
-                    Build.find_by_id(current_build.__id__, function(err, build) {
-                        build.output = build.output + data;
-                        build.save(function(err, key, build){
-                            redis.publish("emerald:Build:" + build.__id__ + ":stdout", data.toString());
-                            redis.publish("Build output", {meta: build.__data__, output: build.output});
-                        });
-                    });
-                });
-                callback(null, command);
-            },
-            function listen_to_stderr(command, callback){
+            function capture_git_stderr(command, args, callback){
                 command.stderr.on('data', function (data) {
                     var regex = /([a-zA-Z0-9 ]+)[:]\s*(\d+[%])/g;
                     var raw_string = data.toString();
@@ -151,33 +150,99 @@ var BuildInstruction = models.declare("BuildInstruction", function(it, kind) {
                             percentage: found[2]
                         }));
                     }
-                    var parsed = found && found[0] || raw_string;
+                });
+                callback(null, command, args);
+            },
+            function handle_the_exit_of_git(command, args, callback) {
+                command.on('exit', function (_code) {
+                    var code = parseInt(_code || 0);
+                    var err = code === 0 ? null : new Error('the build script "git ' + args.join(' ') + '" exited with status ' + _code);
+                    return callback(err, command, args, code);
+                });
+            },
+            function update_build_exit_code(command, args, code, callback){
+                Build.find_by_id(current_build.__id__, function(err, build){
+                    logger.handleException("Build.find_by_id", err);
+                    if (err) {return callback(err);}
+                    build.status = code;
+                    build.save(function(err, key, build){
+                        redis.publish("Repository finished fetching", JSON.stringify({instruction: self.__data__, build: build.__data__}))
+                        callback(null);
+                    });
+                });
+            },
+            function write_build_script(callback){
+                logger.debug('writting build script at ' + script_path);
+                var parts = ["#!/bin/bash"];
+                self.build_script.split(/[\n\r\t\s]*$/gm).forEach(function(line){
+                    parts.push(line.trim() + '; [ $? != 0 ] && exit $?;');
+                });
+                parts.push("exit $?;");
+                var content = parts.join("\n");
+                fs.writeFile(script_path, content, function(err){
+                    callback(err);
+                });
+            },
+            function make_it_writtable(callback){
+                logger.debug('adding execution permission on the build script');
+                fs.chmod(script_path, 0755, function(err){
+                    callback(err);
+                });
+            },
+            function spawn_build_script(callback){
+                logger.info('spawning build script');
+                var args = [script_path];
+                var command = child_process.spawn("bash", args, {cwd: repository_full_path});
+                callback(null, command, args);
+            },
+            function capture_build_stdout (command, args, callback){
+                logger.debug('capturing build script stdout');
+                command.stdout.on('data', function (data) {
                     Build.find_by_id(current_build.__id__, function(err, build) {
-                        build.error = build.error + data;
-                        build.save(function(err, key, build){
-                            redis.publish("emerald:Build:" + build.__id__ + ":stderr", data.toString());
-                            redis.publish("Build output", {meta: build.__data__, output: build.error});
+                        var b = filter_output(data.toString());
+                        build.output = (build.output.indexOf(b) > 0) ? b : (build.output + b);
+
+                        build.save(function(err, key, build) {
+                            redis.publish("Build output", JSON.stringify({meta: build.__data__, output: build.output, instruction: self.__data__}));
                         });
                     });
                 });
-                callback(null, command);
+                callback(null, command, args);
             },
-            function handle_on_exit(command, callback) {
-                command.on('exit', function (_code) {
-                    var code = parseInt(command.pid);
-                    lock.release(function() {
-                        Build.find_by_id(current_build.__id__, function(err, build){
-                            logger.handleException("Build.find_by_id", err);
-                            build.pid = code;
-                            build.save(function(err, key, build){
-                                redis.publish("Repository finished fetching", JSON.stringify(self.__data__))
-                                callback(null);
-                            });
+            function capture_build_stderr (command, args, callback){
+                logger.debug('capturing build script stderr');
+                command.stderr.on('data', function (data) {
+                    console.log(data.toString());
+                    Build.find_by_id(current_build.__id__, function(err, build) {
+                        build.error = filter_output(build.error + data.toString())
+
+                        build.save(function(err, key, build) {
+                            redis.publish("emerald:Build:" + build.__id__ + ":stderr", data.toString());
+
                         });
+                    });
+                });
+                callback(null, command, args);
+            },
+            function handle_the_exit_of_build(command, args, callback) {
+                logger.debug('handling the exit of the command');
+
+                command.on('exit', function (_code) {
+                    var code = parseInt(_code || 0);
+                    logger.info('finished running the build script, code: ' + _code);
+                    var err = code === 0 ? null : new Error('the build script "bash ' + args.join(' ') + '" exited with status ' + _code);
+                    lock.release(function(){
+                        callback(err);
                     });
                 });
             }
-        ]);
+        ], function(err){
+            if (err){
+                lock.release(function(){
+                    logger.fail(err.toString())
+                })
+            }
+        });
     });
 });
 
