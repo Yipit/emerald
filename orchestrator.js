550fa81e4fd408de3707fb0d98b40f414ce7819e
Gabriel Falcao <gabriel@nacaolivre.org>
2011-12-30 13:59:25 -0500

    orchestrator listening to the proper signals

--- orchestrator.js
+++ orchestrator.js
@@ -4,8 +4,11 @@ module.exports.use = function(redis, socket) {
     logger.info("I'm awake");
     const subscribe = require('redis').createClient();
     subscribe.subscribe("BuildInstruction enqueued");
-    subscribe.subscribe("Repository being fetched");
+
+    subscribe.subscribe("Repository started fetching");
     subscribe.subscribe("Repository finished fetching");
+    subscribe.subscribe("Repository being fetched");
+
     subscribe.subscribe("Build started");
     subscribe.subscribe("Build finished");
     subscribe.subscribe("Build output");
@@ -20,7 +23,19 @@ module.exports.use = function(redis, socket) {
         socket.emit(channel, parsed);
         switch (channel) {
             case "BuildInstruction enqueued":
-            logger.success("The BuildInstruction #"+parsed.id+" was successfully enqueued");
+            logger.info("The BuildInstruction #"+parsed.id+" was successfully enqueued");
+                break;
+            case "Repository being fetched":
+            logger.info("The Build #"+parsed.build.__id__+" started fetching changes from the repository");
+                break;
+            case "Repository finished fetched":
+            logger.info("The Build #"+parsed.build.__id__+" finished fetching changes from the repository");
+                break;
+            case "Build started":
+            logger.info("The Build #"+parsed.build.__id__+" has started running");
+                break;
+            case "Build finished":
+            logger.info("The Build #"+parsed.build.__id__+" has finished running");
                 break;
         }
     });
