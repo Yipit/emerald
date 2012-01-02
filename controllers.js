946f2e5f05735e5739fd08ec4e84553ca9d65ec8
Gabriel Falcao <gabriel@nacaolivre.org>
2012-01-02 14:40:12 -0500

    adding url that returns the builds

--- controllers.js
+++ controllers.js
@@ -1,6 +1,6 @@
 var entity = require("./models");
 var settings = require('./settings');
-
+var async = require('async');
 var logger = new (require('./logger').Logger)("[controllers]".yellow);
 
 exports.map = function(app, redis){
@@ -70,6 +70,12 @@ exports.map = function(app, redis){
             });
         });
     });
+
+    app.get('/instructions.json', function(request, response){
+        entity.BuildInstruction.get_latest_with_builds(function(err, instructions){
+            response.send(JSON.stringify(instructions), {'Content-Type': 'application/json'}, 200);
+        });
+    })
 
     app.all('/instructions', function(request, response){
         switch (request.method) {
