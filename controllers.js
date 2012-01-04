d439e856b8b69e86199b53c7f837e721beff4057
Gabriel Falcao <gabriel@nacaolivre.org>
2012-01-04 12:55:57 -0500

    deleting the middleware file, moving context processing into the only controlller that delivers HTML and adding backbone views

--- controllers.js
+++ controllers.js
@@ -2,11 +2,45 @@ var _ = require('underscore')._;
 var entity = require("./models");
 var settings = require('./settings');
 var async = require('async');
+var path = require('path');
+var fs = require('fs');
 var logger = new (require('./logger').Logger)("[controllers]".yellow);
 
 exports.map = function(app, redis){
     app.get('/', function(request, response){
-        response.show('index');
+        async.waterfall([
+            function scan_filesystem(callback){
+                console.log('settings.BACKBONE_VIEW_PATH', settings.BACKBONE_VIEW_PATH)
+                fs.readdir(settings.BACKBONE_VIEW_PATH, callback);
+            },
+            function read_files(files, callback){
+                async.map(files, function(name, callback){
+                    var fullpath = path.join(settings.BACKBONE_VIEW_PATH, name);
+                    fs.readFile(fullpath, function(err, data){
+                        callback(err, {
+                            name: name.replace(/[.]html$/, ''),
+                            html: data.toString()
+                        });
+                    });
+                }, callback);
+            }
+        ], function(err, templates) {
+            logger.handleException(err);
+            var emerald_meta = {
+                domain: settings.EMERALD_DOMAIN,
+                hostname: settings.EMERALD_HOSTNAME,
+                port: settings.EMERALD_PORT
+            };
+            var context = {
+                title: 'Emerald - Continuous Integration',
+                request: request,
+                err: err,
+                templates: templates,
+                emerald: emerald_meta,
+                emerald_json: JSON.stringify(emerald_meta),
+            };
+            return response.render('index', context)
+        });
     });
 
     app.get('/api/instructions.json', function(request, response){
