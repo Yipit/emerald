aad7e2ebe3e57bb1f2422a23454da885e5f334fd
Gabriel Falcao <gabriel@nacaolivre.org>
2012-01-04 22:39:05 -0500

    making InstructionView show when the build is running live

--- public/javascripts/app/views.js
+++ public/javascripts/app/views.js
@@ -1,7 +1,10 @@
 (function($){$(function(){
     function get_template(name){
         var selector = "script#template-" + name;
-        var raw = $(selector).html();
+        var raw = $.trim($(selector).html());
+        if (raw.length == 0){
+            throw new Error('The template "'+name+'" could not be found.');
+        }
         return _.template(raw);
     }
     window.EmeraldView = Backbone.View.extend({
@@ -49,13 +52,62 @@
         tagName: 'li',
         className: 'instruction',
         events: {
-            'click .do-schedule': 'run'
+            'click .do-schedule': 'run',
+            'click .show-output': 'show_output',
+            'click .show-error': 'show_error'
         },
         initialize: function(){
-            _.bindAll(this, 'render');
+            _.bindAll(this, 'render', 'add_build', 'update_latest_build');
             this.model.bind('change', this.render);
-            this.model.bind('build_added', this.render);
+            this.model.bind('build_started', this.add_build);
+            this.model.bind('build_finished', this.update_latest_build);
+
             this.template = get_template('instruction');
+        },
+        /* utility functions */
+        make_build_li: function(build){
+            return [
+                '<li class="' + build.style_name + '" id="clay:Build:id:'+build.__id__+'">',
+                '  <a href="'+build.permalink+'">' + build.message + '</a>',
+                '</li>'
+            ].join('\n');
+        },
+        make_last_build: function(build){
+            return [
+                '<li class="last-build">',
+                '  last build:',
+                '  <strong class="status-color ' + build.style_name + '">',
+                '  <a href="'+build.permalink+'">' + build.message + '</a>',
+                '  </strong>'
+                '</li>'
+            ].join('\n');
+        },
+
+        /* event reactions */
+        add_build: function(build, instruction){
+            this.$("article.instruction").addClass('running');
+            this.$('.buildlog .title').after(this.make_build_li(build));
+        },
+        update_latest_build: function(build, instruction){
+            var element = this.make_build_li(build);
+
+            var $article = this.$("article.instruction");
+            var $last_build = this.$(".last-build");
+            var $li = this.$("li[id='clay:Build:id:" + build.__id__ + "']");
+
+            $article.removeClass('running').addClass(build.style_name);
+
+            $last_build.empty().append(this.make_build_li(build));
+
+            $li.attr('class', build.style_name);
+            $li.find('a').text(build.message);
+        },
+        /* user actions */
+        show_output: function(e){
+            return e.preventDefault();
+        },
+        show_error: function(e){
+            return e.preventDefault();
         },
         run: function(e){
             window.socket.emit('run BuildInstruction', {id: this.model.get('__id__')});
