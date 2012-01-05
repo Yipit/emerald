c36ff0f8b413ebd3900004fc855cba93b4923912
Gabriel Falcao <gabriel@nacaolivre.org>
2012-01-05 02:50:48 -0500

    refactoring controllers

--- controllers.js
+++ controllers.js
@@ -7,47 +7,57 @@ var fs = require('fs');
 var logger = new (require('./logger').Logger)("[controllers]".yellow);
 
 exports.map = function(app, redis){
-    app.get('/', function(request, response){
-        async.waterfall([
-            function scan_filesystem(callback){
-                fs.readdir(settings.BACKBONE_VIEW_PATH, callback);
-            },
-            function filter_for_valid_filenames(files, callback){
-                var pattern = /^\w+.*[.]html$/i;
+    function controller (_callback_) {
+        return function (request, response){
+            async.waterfall([
+                function scan_filesystem(callback){
+                    fs.readdir(settings.BACKBONE_VIEW_PATH, callback);
+                },
+                function filter_for_valid_filenames(files, callback){
+                    var pattern = /^\w+.*[.]html$/i;
 
-                return callback(null, _.filter(files, function(file){
-                    return pattern.test(file);
-                }));
-            },
-            function read_files(files, callback){
-                async.map(files, function(name, callback){
-                    var fullpath = path.join(settings.BACKBONE_VIEW_PATH, name);
-                    fs.readFile(fullpath, function(err, data){
-                        callback(err, {
-                            name: name.replace(/[.]html$/, ''),
-                            html: data.toString()
+                    return callback(null, _.filter(files, function(file){
+                        return pattern.test(file);
+                    }));
+                },
+                function read_files(files, callback){
+                    async.map(files, function(name, callback){
+                        var fullpath = path.join(settings.BACKBONE_VIEW_PATH, name);
+                        fs.readFile(fullpath, function(err, data){
+                            callback(err, {
+                                name: name.replace(/[.]html$/, ''),
+                                html: data.toString()
+                            });
                         });
-                    });
-                }, callback);
-            }
-        ], function(err, templates) {
-            logger.handleException(err);
-            var emerald_meta = {
-                domain: settings.EMERALD_DOMAIN,
-                hostname: settings.EMERALD_HOSTNAME,
-                port: settings.EMERALD_PORT
-            };
-            var context = {
-                title: 'Emerald - Continuous Integration',
-                request: request,
-                err: err,
-                templates: templates,
-                emerald: emerald_meta,
-                emerald_json: JSON.stringify(emerald_meta),
-            };
-            return response.render('index', context)
-        });
-    });
+                    }, callback);
+                }
+            ], function(err, templates) {
+                logger.handleException(err);
+                var emerald_meta = {
+                    domain: settings.EMERALD_DOMAIN,
+                    hostname: settings.EMERALD_HOSTNAME,
+                    port: settings.EMERALD_PORT
+                };
+                var context = {
+                    title: 'Emerald - Continuous Integration',
+                    request: request,
+                    err: err,
+                    templates: templates,
+                    emerald: emerald_meta,
+                    emerald_json: JSON.stringify(emerald_meta),
+                };
+                return _callback_(err, context, request, response);
+            });
+        }
+    }
+    app.get('/', controller(function(err, context, request, response){
+        return response.render('index', context)
+    }));
+
+    app.get('/_design', controller(function(err, context, request, response){
+        return response.render('design', context)
+    }));
+
 
     app.get('/api/instructions.json', function(request, response){
         entity.BuildInstruction.get_latest_with_builds(function(err, instructions){
