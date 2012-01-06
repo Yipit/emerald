23e1a09662e3b72ba08cc5bbc5f2cff3f499b6b8
Gabriel Falcao <gabriel@nacaolivre.org>
2012-01-05 20:03:25 -0500

    showing "gritter" when a build is aborted

--- public/javascripts/app/sockets.js
+++ public/javascripts/app/sockets.js
@@ -8,11 +8,19 @@
         });
     });
 
-    socket.on('BuildInstruction enqueued', function(data){
+    window.socket.on('BuildInstruction enqueued', function(data){
         $.gritter.add({
             title: '"' + data.name + '" was enqueued',
             text: "The build instruction #" + data.id + " has been added to the queue",
             image: window.emerald.domain + "/images/control_double_down.png"
+        });
+    });
+
+    window.socket.on('Build aborted', function(data){
+        $.gritter.add({
+            title: '"' + data.instruction.name + '" was aborted',
+            text: "The build instruction #" + data.instruction.id + " has been successfully aborted",
+            image: window.emerald.domain + "/images/ico_abort.png"
         });
     });
 })(jQuery);
