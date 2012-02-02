4f36a7ec430f5307d102c0e83f7d03efe38b9d0c
Gabriel Falcao <gabriel@nacaolivre.org>
2012-02-02 11:02:48 -0500

    binding the proper socket.io events into Backbone events

--- app/client/models.js
+++ app/client/models.js
@@ -40,12 +40,14 @@
                 }
             }
             window.socket.on('Build started',                bypass_signal("Build started",                "build_started"));
+            window.socket.on('Build running',                bypass_signal("Build running",                "build_running"));
             window.socket.on('Build finished',               bypass_signal("Build finished",               "build_finished"));
             window.socket.on('Build aborted',                bypass_signal("Build aborted",                "build_aborted"));
             window.socket.on('Build output',                 bypass_signal("Build output",                 "build_output"));
 
+            window.socket.on('Repository started fetching', bypass_signal("Repository started fetching", "repository_fetched"));
             window.socket.on('Repository being fetched',     bypass_signal("Repository being fetched",     "fetching_repository"));
-            window.socket.on('Repository finished fetching', bypass_signal("Repository finished fetching", "fetching_repository"));
+            window.socket.on('Repository finished fetching', bypass_signal("Repository finished fetching", "repository_fetched"));
         }
     });
 
