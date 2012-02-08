7da879ab92c24e2df3803c3e90a7d3e81472a913
Gabriel Falcao <gabriel@nacaolivre.org>
2012-02-08 15:58:27 -0500

    preparing to migrate to backbone 0.9

--- app/client/app.js
+++ app/client/app.js
@@ -3,5 +3,12 @@
     window.main_console = new window.ConsoleSource();
 
     window.App = new window.EmeraldRouter();
+    // Backbone.history.bind('route', function(router, controller_name){
+    //     var selector = "#controller-" + controller_name;
+    //     $(selector).siblings().removeClass("active");
+    //     $(selector).addClass("active");
+    // });
+
     Backbone.history.start();
+
 });})(jQuery);
