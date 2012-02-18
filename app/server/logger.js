77e2c3e38117ade4ea449a526046ebfe4f516613
Gabriel Falcao <gabriel@nacaolivre.org>
2012-02-17 20:28:36 -0500

    avoiding mangled output of ASCII chars when not in a TTY

--- app/server/logger.js
+++ app/server/logger.js
@@ -16,7 +16,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************************************************/
-require('colors');
 var _ = require('underscore')._;
 var settings = require('../../settings');
 var loglevel = {
--- app/terminal/generate-data.js
+++ app/terminal/generate-data.js
@@ -16,7 +16,11 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************************************************/
-require("colors");
+var colors = require('colors');
+if (!process.stdout.isTTY) {
+    colors.mode = 'none';
+}
+
 
 var entity = require('../server/models');
 GLOBAL.settings = require('../../settings');
