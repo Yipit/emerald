46b80f2802203eb8c46c6d58a534aa72485214c3
Lincoln de Sousa <lincoln@comum.org>
2012-04-19 11:12:14 -0300

    Increasing the memory amount that each build script can take

--- app/server/command.js
+++ app/server/command.js
@@ -23,6 +23,11 @@ module.exports.dispatch = function (command, args, callback) {
     var self = this;
     var spawn_args = [];
 
+    spawn_args.push('--max_new_space_size=5000');
+    spawn_args.push('--max_executable_size=3000');
+    spawn_args.push('--max_old_space_size=5000');
+    spawn_args.push(settings.SCRIPT_PATH);
+
     /* Looking for the settings file that the current process is using */
     if (settings.CUSTOM) {
         spawn_args.push('-s');
@@ -34,7 +39,10 @@ module.exports.dispatch = function (command, args, callback) {
     /* Putting all parameters together */
     spawn_args = _.union(spawn_args, args);
 
-    var child = spawn(settings.SCRIPT_PATH, spawn_args);
+    console.log("spawn_args");
+    console.log(spawn_args);
+
+    var child = spawn("node", spawn_args);
     child.stdout.on('data', function(data) {
         process.stdout.write(data);
     });
